43fd749d2c92c4da62a8c7789eb1664779ed4bf5
SeYeong-kang <144921786+SeYeong-kang@users.noreply.github.com>
2024-01-07 12:19:53 +0900

    Update EnergyTier.py

--- StorageVET-1.2.3/storagevet/Tariff/EnergyTier.py
+++ StorageVET-1.2.3/storagevet/Tariff/EnergyTier.py
@@ -29,42 +29,50 @@ Copyright (c) 2023, Electric Power Research Institute
 """
 class Tier:
     def __init__(self, number, max, rate, unit, adj, sell):
-        self.number = number
+        self.number = number 
         self.max = max
         self.rate = rate
         self.adj = adj
         self.unit = unit
         self.sell = sell
+     #  number (int): 티어의 번호 또는 식별자.
+     # max: (float): 최대 사용량.
+     # rate (float): 사용량에 대한 요금 비율.
+     # unit (str): 사용량의 단위.
+     # adj (float): 조절값(adjustment).
+     # sell (float): 판매 비율.
 
     def tostring(self):
         """
         Pretty print all necessary tier information
 
         """
+        # 최대 사용량 정보 출
         if self.max is not None:
             if self.unit is not None:
                 print("Tier " + str(self.number) + ": Max Usage: " + str(self.max) + " " + self.unit)
             else:
                 print("Tier " + str(self.number) + ": Max Usage: " + str(self.max))
-
+        # 요금 비율 정보 출력
         if self.rate is not None:
             if self.unit is not None:
                 print("Tier " + str(self.number) + ": Rate: $" + str(self.rate) + " /" + self.unit)
             else:
                 print("Tier " + str(self.number) + ": Rate: $" + str(self.rate))
-
+       # 조절값 정보 출력
         if self.adj is not None:
             if self.unit is not None:
                 print("Tier " + str(self.number) + ": Adjustments: $" + str(self.adj) + " /" + self.unit)
             else:
                 print("Tier " + str(self.number) + ": Adjustments: $" + str(self.adj))
-
+        # 판매 비율 정보 출력
         if self.sell is not None:
             if self.unit is not None:
                 print("Tier " + str(self.number) + ": Sell: $" + str(self.sell) + " /" + self.unit)
             else:
                 print("Tier " + str(self.number) + ": Sell: $" + str(self.sell))
-
+             
+    # 해당 티어의 요금 비율을 반환하는 기능
     def get_rate(self):
         """
         Returns:
